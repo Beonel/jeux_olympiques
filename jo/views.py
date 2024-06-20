5bfa68b6df007761abde37f37dcc705a421188f2
Beonel <beomed.jean@gmail.com>
2024-06-20 21:04:46 +0200

    Fix import errors and add ajouter_au_panier view

--- jo/views.py
+++ jo/views.py
@@ -18,5 +18,20 @@ def login(request):
 def register(request):
     return render(request, 'register.html')
 
+def ajouter_au_panier(request):
+    return render(request, 'ajouter_au_panier.html')
+
+def supprimer_du_panier(request):
+    return render(request, 'supprimer_du_panier.html')
+
+def inscription(request):
+    return render(request, 'inscription.html')
+
+def connexion(request):
+    return render(request, 'connexion.html')
+
+def deconnexion(request):
+    return render(request, 'deconnexion.html')
+
 
 # Create your views here.
