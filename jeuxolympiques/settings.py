e264159d831fa6eb08838e99978c81bece95a19c
Jean97483 <jean.beonel@outlook.fr>
2024-06-21 09:23:05 +0200

    Configured PostgreSQL database

--- jeuxolympiques/settings.py
+++ jeuxolympiques/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'jeuxolympiques.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-import dj_database_url
+
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
